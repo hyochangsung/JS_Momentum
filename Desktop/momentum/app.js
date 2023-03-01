a1f217c5834c22de8252c3f0bba75ab814f83fb9
hyochangsung <xx1733@naver.com>
2023-03-01 21:39:57 +0900

    #4.6 if문을 활용한 username 저장법

--- Desktop/momentum/app.js
+++ Desktop/momentum/app.js
@@ -1,16 +1,27 @@
-const loginForm = document.querySelector(".login-form");
-const loginInput = document.querySelector(".login-form input");
-const loginButton = document.querySelector(".login-form button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onSubmit);
+function paintGreeting(username){
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+greeting.innerText = `Hello ${username}`;
+}
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onSubmit);
+}else{
+  paintGreeting(savedUserName);
+}
